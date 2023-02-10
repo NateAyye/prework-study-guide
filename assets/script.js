0c1a26937033639b8f78ecde4d98cd43f8f3a08a
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-02-09 19:09:52 -0800

    Added the base JavaScript in the project

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+let shape = "square";
+
+let topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+// let shapes = ["triangle", "square", "pentagon", "circle"];
+//
+// for (let x = 0; x < shapes.length; x++) {
+//   console.log(shapes[x]);
+// }
+
+// let y = 0;
+// while (y < shapes.length) {
+//   console.log(shapes[y]);
+//   y++;
+// }
+let studentInfo = ["Lu", 54, true];
+
+let topic = "HTML";
+
+for (var x = 0; x < topics.length; x++) {
+  console.log(topics[x]);
+}
+
+// if (topic === "HTML") {
+//   console.log("Let's study HTML!");
+// } else if (topic === "CSS") {
+//   console.log("Let's study CSS!");
+// } else if (topic === "Git") {
+//   console.log("Let's study Git!");
+// } else if (topic === "JavaScript") {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log("Please try again!");
+// }
